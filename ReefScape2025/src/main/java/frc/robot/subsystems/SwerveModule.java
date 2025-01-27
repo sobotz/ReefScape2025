7f443eff74dd3c627a5aef5041fc051f636ea5eb
Calum Tjon <astarfall1288@gmail.com>
2025-01-27 08:51:29 -0500

    Initial commit

---
+++ ReefScape2025/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,145 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.swerve.SimSwerveDrivetrain;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.Constants;
+
+public class SwerveModule {
+    //motors
+    TalonFX driveMotor;
+    TalonFX turnMotor;
+    //CAN coder
+    CANcoder axisSensor;
+    //degree when rotating
+    double rotationalDegreeValue;
+    //vector used for calculating the exact angle and magnitude
+    Vector strafeVector;
+    Vector rotationalVector;
+    Vector driveVector;
+    //Current module values
+    double currentModuleDegree;
+    //Target module values
+    double targetModuleDegree;
+    double targetModuleMagnitude;
+
+    double degreeOffset;
+    //PID Controller
+    PIDController degreeController;
+    PIDController velocityController;
+    
+
+    boolean inverted;
+    public SwerveModule(TalonFX dMotor, TalonFX tMotor, CANcoder aSensor, double rotationalDegreeValue,double degreeOffset){
+        //Motors
+        this.driveMotor = dMotor;
+        this.turnMotor = tMotor;
+        //CAN coder from swerve subsystem
+        this.axisSensor = aSensor;
+        //value when rotating
+        this.rotationalDegreeValue = rotationalDegreeValue;
+        //driving vectors
+        strafeVector = new Vector(0,0);
+        rotationalVector = new Vector(0,0);
+        //PID 
+        degreeController = new PIDController(0.01, 0.009,0.0012);/// tuned properly make sure the calculate is about 0
+        //degreeController.setTolerance(.05);
+        degreeController.enableContinuousInput(0,360);
+        //inverted module wheel direction and magnitude
+        inverted = false;
+        //CANcoder offset to have 0 degrees align with the robot
+        this.degreeOffset = degreeOffset;
+    }
+    public void drive(Vector strafeVector, double rotationalMagnitude, double currentRobotDegree, boolean relativeVelocityControl){
+        //setting and creating the strafe vector and rotational vector
+        //System.out.println(relativeVelocityControl);
+        this.strafeVector = strafeVector;
+        this.rotationalVector = new Vector(rotationalMagnitude, rotationalDegreeValue,true);
+        
+        
+        /*if (Math.abs(rotationalVector.getMagnitude())>0.5){
+            rotationalVector.setMagnitude(0.5 *(rotationalVector.getMagnitude()/Math.abs(rotationalVector.getMagnitude())));
+            if (strafeVector.getMagnitude()>0.5){
+                strafeVector.setMagnitude(0.5 * (strafeVector.getMagnitude()/Math.abs(strafeVector.getMagnitude())));
+            }
+        }
+        else{ 
+            if ((strafeVector.getMagnitude() + rotationalVector.getMagnitude())>1){
+                strafeVector.setMagnitude(1-Math.abs(rotationalVector.getMagnitude()));
+            }
+            
+        }*/
+
+        //combining vectors
+        //System.out.println(relativeVelocityControl);
+        if(relativeVelocityControl == false){
+            
+            strafeVector = new Vector(strafeVector.getMagnitude(),strafeVector.getDegrees()-currentRobotDegree,true);
+        }
+        
+        //System.out.println(currentRobotDegree);
+        driveVector = strafeVector.addVector(rotationalVector);
+        //driveVector = strafeVector;
+        //axisSensor.getAbsolutePosition();
+        //driveVector = strafeVector;
+        //current wheel degree  0 offsetted
+        currentModuleDegree = (((((axisSensor.getPosition().getValueAsDouble()) % 1) + 1) % 1) * 360);
+        currentModuleDegree = (((currentModuleDegree - degreeOffset) + 360) % 360);
+
+        targetModuleMagnitude = driveVector.getMagnitude();
+        targetModuleDegree = driveVector.getDegrees();
+            if (inverted){
+                targetModuleMagnitude *= -1;
+                targetModuleDegree = ((targetModuleDegree + 180)%360);
+            }
+        
+        //field orientation
+        
+        //the target values put into variables from a vector;
+    
+        //need to call calculate to determine the degree error
+        degreeController.calculate(currentModuleDegree,targetModuleDegree);
+        //if rotation is greater than 90 then inverted true
+        if (Math.abs(degreeController.getError())>90){
+            inverted = !inverted;
+        }
+        //PID controller
+        //System.out.println(degreeController.calculate(currentModuleDegree,targetModuleDegree));
+        turnMotor.set(-degreeController.calculate(currentModuleDegree,targetModuleDegree));
+        driveMotor.set(targetModuleMagnitude);
+        //System.out.println(currentModuleDegree);
+        
+    }
+    public double getRawValue(){
+        return (((((axisSensor.getPosition().getValueAsDouble()) % 1) + 1) % 1) * 360);
+    }
+    public double getTarget(){
+        return targetModuleDegree;
+    }
+    public double getDriveSensorPosition(){
+        return driveMotor.getPosition().getValueAsDouble()/25.97290039;//* Constants.SwerveConstants.wheelRotationPerMotorRotation*(2 * Math.PI* Constants.SwerveConstants.wheelRadius);//CHANGE
+    }
+    public double getRawDriveSensorPosition(){
+        return driveMotor.getPosition().getValueAsDouble();
+    }
+    public double getDriveSensorVelocity(){
+        return (driveMotor.getVelocity().getValueAsDouble()/25.97290039);// * Constants.SwerveConstants.wheelRotationPerMotorRotation)/60) * 2 * Math.PI * Constants.SwerveConstants.wheelRadius);//CHANGE
+    }
+    public double getCurrentModuleDegree(){
+        return currentModuleDegree;
+    }
+    public Rotation2d get2dCurrentModuleDegree(){
+        Rotation2d x = new Rotation2d((currentModuleDegree * Math.PI)/180);
+        return x;
+    }
+    public SwerveModulePosition getSwerveModulePosition(){
+        return new SwerveModulePosition((getDriveSensorPosition()),get2dCurrentModuleDegree());
+    }
+    public SwerveModuleState getSwerveModuleState(){
+        return new SwerveModuleState(getDriveSensorVelocity(), get2dCurrentModuleDegree());// speed meters per second , rotation 2d angle
+    }
+}
---
+++ ReefScape2025/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,353 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class SwerveSubsystem extends SubsystemBase {
+  //FRONT LEFT MOTORS
+  TalonFX frontLeftDriveMotor;
+  TalonFX frontLeftTurnMotor;
+  //FRONT RIGHT MOTORS
+  TalonFX frontRightDriveMotor;
+  TalonFX frontRightTurnMotor;
+  //BACK LEFT MOTORS
+  TalonFX backLeftDriveMotor;
+  TalonFX backLeftTurnMotor;
+  //BACK RIGHT MOTORS
+  TalonFX backRightDriveMotor;
+  TalonFX backRightTurnMotor;
+  //SWERVE MODULE CAN ENCODERS 
+  CANcoder frontLeftEncoder;
+  CANcoder frontRightEncoder;
+  CANcoder backLeftEncoder;
+  CANcoder backRightEncoder;
+  //GYROSCOPE PIGEON 
+  Pigeon2 robotGyro;
+  //SWERVE MODULES 
+  SwerveModule frontLeftSwerveModule;
+  SwerveModule frontRightSwerveModule;
+  SwerveModule backLeftSwerveModule;
+  SwerveModule backRightSwerveModule;
+  //PID CONTROLLERS
+  PIDController rotationController;
+  PIDController angleCorrectionController;
+  PIDController xTranslationController;
+  PIDController yTranslationController;
+  PIDController xVelocityController;
+  PIDController yVelocityController;
+  PIDController degreeVelocityController;
+  //CURRENT STATUS VARIABLES
+  double currentRobotDegree;
+  double robotDegreeOffset;
+  double xVelocity;
+  double yVelocity;
+  double xTranslation;
+  double yTranslation;
+  double rotationalVelocityMagnitude;
+
+  boolean angleCorrectionMode;
+  boolean once;
+  boolean driveCommandDisabled;
+
+  Translation2d m_frontLeftLocation;
+  Translation2d m_frontRightLocation;
+  Translation2d m_backLeftLocation;
+  Translation2d m_backRightLocation;
+  Rotation2d autoRobotDegree;
+
+  SwerveDriveKinematics m_kinematics;
+  SwerveDriveOdometry m_odometer;
+  ChassisSpeeds chassisSpeed;
+  RobotConfig config;
+  /*Update requirements
+   * *******SWERVE SUBSYSTEM*******
+   * ID every device
+   * xTranslational PID controller needs tuning  
+   * yTranslational PID controller needs tuning
+   * xVelocity PID controller needs tuning
+   * yVelocity PID controller needs tuning
+   * rotationalVelocity PID controller needs tuning
+   * drivetrain motors may need to be set to the correct canbus
+   * RotationalValueDegree inputted as a parameter into the swerve modules needs to be updated (robot is not square)
+   * ------------------------------
+   * *******SWERVE MODULE*******
+   * getPosition for drive motor needs to be checked
+   * getVelocity for drive motor needs to be checked
+   * degree PID Controller needs to be tuned
+   * ------------------------------
+   * *******PATHPLANNER GUI*******
+   * robot stats needs to be updated
+   * all robot limits needs to be tested and implements(maximize speed without losing accuracy)
+   */
+
+
+  public SwerveSubsystem() {
+    //SWERVE MOTORS INSTANTIATION
+    frontLeftDriveMotor = new TalonFX(2);
+    frontLeftTurnMotor = new TalonFX(1);
+    frontRightDriveMotor = new TalonFX(4);
+    frontRightTurnMotor = new TalonFX(3);
+    backLeftDriveMotor = new TalonFX(8);
+    backLeftTurnMotor = new TalonFX(7);
+    backRightDriveMotor = new TalonFX(6);
+    backRightTurnMotor = new TalonFX(5);
+    //ENCODER INSTANTIATION
+    frontLeftEncoder = new CANcoder(21);
+    frontRightEncoder = new CANcoder(22);
+    backLeftEncoder = new CANcoder(24);
+    backRightEncoder = new CANcoder(23);
+    //PIGEON2 INSTANTIATION
+    robotGyro = new Pigeon2(25);
+    //SWERVE MODULE INSTANTIATION
+    frontLeftSwerveModule = new SwerveModule(frontLeftDriveMotor,frontLeftTurnMotor,frontLeftEncoder,315,Constants.SwerveConstants.frontLeftDegreeOffset);
+    frontRightSwerveModule = new SwerveModule(frontRightDriveMotor,frontRightTurnMotor,frontRightEncoder,225,Constants.SwerveConstants.frontRightDegreeOffset);
+    backLeftSwerveModule = new SwerveModule(backLeftDriveMotor,backLeftTurnMotor,backLeftEncoder,45,Constants.SwerveConstants.backLeftDegreeOffset);
+    backRightSwerveModule = new SwerveModule(backRightDriveMotor,backRightTurnMotor,backRightEncoder,135,Constants.SwerveConstants.backRightDegreeOffset);
+    //PID CONTROLLER INSTANTIATION
+    angleCorrectionController = new PIDController(0,0,0);
+    //Connects 0 degrees to 360 degrees to allow for the least distance error from current to target
+    angleCorrectionController.enableContinuousInput(0,360);
+    //Allows for leeway if the current degree is not exactly on target
+    angleCorrectionController.setTolerance(0.2);
+
+    rotationController = new PIDController(0.0043,0,0.00);
+    rotationController.enableContinuousInput(0,360); 
+    rotationController.setTolerance(0.2);
+
+    xTranslationController = new PIDController(0,0,0);
+    xTranslationController.setTolerance(0.01);
+
+    yTranslationController = new PIDController(0,0,0);
+    yTranslationController.setTolerance(0.01);
+
+    xVelocityController = new PIDController(0.023,0,0.001);
+    //xVelocityController.setTolerance(0.01);
+    
+    yVelocityController = new PIDController(0.023,0,0.001);
+    //yVelocityController.setTolerance(0.01);
+
+    degreeVelocityController = new PIDController(0.011,0.000,0.007);//d0.07
+    //degreeVelocityController.setTolerance(0.01);
+
+    once = true;
+    robotDegreeOffset = 0;
+    driveCommandDisabled = false;
+    rotationalVelocityMagnitude =0;
+    m_frontLeftLocation = new Translation2d(0.355, 0.355);
+    m_frontRightLocation = new Translation2d(0.355, -0.355);
+    m_backLeftLocation = new Translation2d(-0.355, 0.355);
+    m_backRightLocation = new Translation2d(-0.355, -0.355);
+
+    m_kinematics = new SwerveDriveKinematics(m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
+    autoRobotDegree = new Rotation2d();
+    robotGyro.reset();
+    m_odometer = new SwerveDriveOdometry(
+      m_kinematics,
+      autoRobotDegree,
+      new SwerveModulePosition[]{
+      frontLeftSwerveModule.getSwerveModulePosition(),
+      frontRightSwerveModule.getSwerveModulePosition(),
+      backLeftSwerveModule.getSwerveModulePosition(),
+      backRightSwerveModule.getSwerveModulePosition()}
+    );
+    chassisSpeed = new ChassisSpeeds();
+    
+    try{
+      config = RobotConfig.fromGUISettings();
+    } catch (Exception e) {
+      // Handle exception as needed
+      e.printStackTrace();
+    }
+    
+    AutoBuilder.configure(
+            this::getPose, // Robot pose supplier
+            this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+            this::getSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            this::velocityControlledDrive, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+            new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+                    new PIDConstants(5, 0.0, 0.0), // Translation PID constants
+                    new PIDConstants(5, 0.0, 0.0) // Rotation PID constants
+            ),
+            config, // The robot configuration
+            () -> {
+              // Boolean supplier that controls when the path will be mirrored for the red alliance
+              // This will flip the path being followed to the red side of the field.
+              // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+              var alliance = DriverStation.getAlliance();
+              if (alliance.isPresent()) {
+                return alliance.get() == DriverStation.Alliance.Red;
+              }
+              return false;
+            },
+            this // Reference to this subsystem to set requirements
+    );
+  }
+  public double getCurrentRobotDegree(){
+    return currentRobotDegree;
+  }
+  /*public void setCorrectionMode(boolean mode){
+    angleCorrectionMode = mode;
+  }*/
+  public void setDriveCommandDisabled(boolean mode){
+    driveCommandDisabled = mode;
+  }
+  public void driverControlledDrive(Vector strafeVector, Vector rotationVector){
+    if (!driveCommandDisabled){
+      drive(strafeVector,rotationVector,false);
+    }
+  }
+  public void velocityControlledDrive(ChassisSpeeds targetSpeeds){
+    //System.out.println("VX: " + targetSpeeds.vxMetersPerSecond);
+    //System.out.println("VY: " + targetSpeeds.vyMetersPerSecond);
+    //System.out.println((Math.atan2(targetSpeeds.vyMetersPerSecond,targetSpeeds.vxMetersPerSecond)*(180/Math.PI)));
+    xVelocity += xVelocityController.calculate(getSpeeds().vxMetersPerSecond,targetSpeeds.vxMetersPerSecond);//ACTUTALY IN AUTO ITS FORWARD IS POSITIVE
+    yVelocity += yVelocityController.calculate(getSpeeds().vyMetersPerSecond,targetSpeeds.vyMetersPerSecond);//ACTUTALLY IN AUTO LEFT IS POSITIVE
+    //System.out.println("x Velocity: " + xVelocityController.calculate(getSpeeds().vxMetersPerSecond,targetSpeeds.vxMetersPerSecond));
+    //System.out.println("y Velocity: " + yVelocityController.calculate(getSpeeds().vyMetersPerSecond,targetSpeeds.vyMetersPerSecond));
+    //System.out.println(yVelocity);
+    System.out.println("current: " + getSpeeds().omegaRadiansPerSecond);
+    System.out.println("target: " + targetSpeeds.omegaRadiansPerSecond);
+    //System.out.println("CalcTEST: " + -degreeVelocityController.calculate(1,0.1));
+    System.out.println("calc: " + -degreeVelocityController.calculate(/*robotGyro.getAngularVelocityZWorld().getValueAsDouble() * (Math.PI/180)*/getSpeeds().omegaRadiansPerSecond,targetSpeeds.omegaRadiansPerSecond));
+    Vector strafeVector = new Vector(-yVelocity, xVelocity);
+    rotationalVelocityMagnitude += -degreeVelocityController.calculate(getSpeeds().omegaRadiansPerSecond,targetSpeeds.omegaRadiansPerSecond);
+    if (targetSpeeds.vxMetersPerSecond == 0 && targetSpeeds.vyMetersPerSecond == 0 && targetSpeeds.omegaRadiansPerSecond == 0){
+      strafeVector = new Vector(0,0);
+      rotationalVelocityMagnitude = 0;
+    }
+    frontLeftSwerveModule.drive(strafeVector, rotationalVelocityMagnitude, currentRobotDegree,true);
+    frontRightSwerveModule.drive(strafeVector, rotationalVelocityMagnitude, currentRobotDegree,true);
+    backLeftSwerveModule.drive(strafeVector, rotationalVelocityMagnitude, currentRobotDegree,true);
+    backRightSwerveModule.drive(strafeVector, rotationalVelocityMagnitude, currentRobotDegree,true);
+  }
+  public void reefControlledDrive(double xOffset, double yOffset, double xTarget, double yTarget){}
+
+  public void drive(Vector strafeVector, Vector rotationVector,boolean relativeVelocityControlled){
+    double rotationalMagnitude = -rotationController.calculate(currentRobotDegree,rotationVector.getDegrees());
+    if (Math.abs(rotationalMagnitude) < 0.01){
+      rotationalMagnitude = 0;
+    }
+    frontLeftSwerveModule.drive(strafeVector, rotationalMagnitude, currentRobotDegree,relativeVelocityControlled);
+    frontRightSwerveModule.drive(strafeVector, rotationalMagnitude, currentRobotDegree,relativeVelocityControlled);
+    backLeftSwerveModule.drive(strafeVector, rotationalMagnitude, currentRobotDegree,relativeVelocityControlled);
+    backRightSwerveModule.drive(strafeVector, rotationalMagnitude, currentRobotDegree,relativeVelocityControlled);
+  }
+  public void rotationMagnitudeDrive(Vector strafeVector, double rotationalMagnitude, double currentRobotDegree, boolean relativeVelocityControlled){
+    frontLeftSwerveModule.drive(strafeVector, rotationalMagnitude, currentRobotDegree,relativeVelocityControlled);
+    frontRightSwerveModule.drive(strafeVector, rotationalMagnitude, currentRobotDegree,relativeVelocityControlled);
+    backLeftSwerveModule.drive(strafeVector, rotationalMagnitude, currentRobotDegree,relativeVelocityControlled);
+    backRightSwerveModule.drive(strafeVector, rotationalMagnitude, currentRobotDegree,relativeVelocityControlled);
+  }
+  public Pose2d getPose() {
+    return m_odometer.getPoseMeters();
+  }
+
+  public void resetPose(Pose2d pose) {
+    m_odometer.resetPosition(autoRobotDegree, getAllSwerveModulePositions(), pose);
+  }
+
+  public SwerveModulePosition[] getAllSwerveModulePositions(){
+    return new SwerveModulePosition[]{
+      frontLeftSwerveModule.getSwerveModulePosition(),
+      frontRightSwerveModule.getSwerveModulePosition(),
+      backLeftSwerveModule.getSwerveModulePosition(),
+      backRightSwerveModule.getSwerveModulePosition()};
+  }
+  public SwerveModuleState[] getAllSwerveModuleStates(){
+    return new SwerveModuleState[]{
+      frontLeftSwerveModule.getSwerveModuleState(),
+      frontRightSwerveModule.getSwerveModuleState(),
+      backLeftSwerveModule.getSwerveModuleState(),
+      backRightSwerveModule.getSwerveModuleState()
+    };
+  }
+
+  public ChassisSpeeds getSpeeds() {
+    return m_kinematics.toChassisSpeeds(getAllSwerveModuleStates());
+    
+  }
+  @Override
+  public void periodic() {
+    ChassisSpeeds speed = getSpeeds();
+    if (currentRobotDegree > 180){
+      autoRobotDegree = new Rotation2d((currentRobotDegree - 360) * (Math.PI/180));
+    }
+    else{
+      autoRobotDegree = new Rotation2d(currentRobotDegree * (Math.PI/180));
+    }
+    //autoRobotDegree = new Rotation2d(currentRobotDegree * (Math.PI/180));
+    
+
+    //Set the current degree of the robot 
+    if (once){
+      robotDegreeOffset = ((((robotGyro.getYaw().getValueAsDouble()) % 360) + 360) % 360);
+      once = false;
+      robotGyro.reset();
+    }
+    currentRobotDegree = ((((robotGyro.getYaw().getValueAsDouble() - robotDegreeOffset) % 360) + 360) % 360);
+    m_odometer.update(autoRobotDegree,new SwerveModulePosition[]{
+      frontLeftSwerveModule.getSwerveModulePosition(),
+      frontRightSwerveModule.getSwerveModulePosition(),
+      backLeftSwerveModule.getSwerveModulePosition(),
+      backRightSwerveModule.getSwerveModulePosition()}
+    );
+    // SmartDashboard.putNumber("front left",frontLeftSwerveModule.getSwerveModulePosition().angle.getDegrees());
+    // SmartDashboard.putNumber("front Right",frontRightSwerveModule.getSwerveModulePosition().angle.getDegrees());
+    // SmartDashboard.putNumber("back left",backLeftSwerveModule.getSwerveModulePosition().angle.getDegrees());
+    // SmartDashboard.putNumber("back Right",backRightSwerveModule.getSwerveModulePosition().angle.getDegrees());
+    
+    //SmartDashboard.putNumber("Front Left", frontLeftSwerveModule.getRawValue());
+    //SmartDashboard.putNumber("Front Right", frontRightSwerveModule.getRawValue());
+    //SmartDashboard.putNumber("Back Left", backLeftSwerveModule.getRawValue());
+    //SmartDashboard.putNumber("Back Right", backRightSwerveModule.getRawValue());
+    
+    //SmartDashboard.putNumber("degrees", autoRobotDegree.getDegrees());
+    SmartDashboard.putNumber("x Trans", m_odometer.getPoseMeters().getX());
+    SmartDashboard.putNumber("y Trans", m_odometer.getPoseMeters().getY());
+    SmartDashboard.putNumber("x Vel", getSpeeds().vxMetersPerSecond);
+    SmartDashboard.putNumber("y Vel", getSpeeds().vyMetersPerSecond);
+    SmartDashboard.putNumber("raw drive sensor", frontLeftSwerveModule.getRawDriveSensorPosition());
+    SmartDashboard.putNumber("Angular Velocity",robotGyro.getAngularVelocityZWorld().getValueAsDouble() * (Math.PI/180));
+    SmartDashboard.putNumber("degree",currentRobotDegree);
+    
+    // SmartDashboard.putNumber("Left Drive Encoder", frontLeftSwerveModule.getDriveSensorPosition());
+    //SmartDashboard.putNumber("Front Left Drive Position", frontLeftSwerveModule.getDriveSensorPosition());
+    //SmartDashboard.putNumber("Front Left Drive Velocity", frontLeftSwerveModule.getDriveSensorVelocity());
+    //SmartDashboard.putNumber("BackRight Degree:", backRightSwerveModule.get2dCurrentModuleDegree().getDegrees());
+    //System.out.println(backRightSwerveModule.get2dCurrentModuleDegree().getDegrees());
+    /*chassisSpeed = m_kinematics.toChassisSpeeds(
+      frontLeftSwerveModule.getSwerveModuleState(),
+      frontRightSwerveModule.getSwerveModuleState(),
+      backLeftSwerveModule.getSwerveModuleState(),
+      backRightSwerveModule.getSwerveModuleState()
+    );*/
+    chassisSpeed = speed;
+  }
+}
