a80daf34c1fbb25ba99cbea459378599dd7006d1
Calum Tjon <astarfall1288@gmail.com>
2025-02-25 08:57:48 -0500

    save

--- ReefScape2025/src/main/java/frc/robot/commands/SetElevatorL1Command.java
+++ ReefScape2025/src/main/java/frc/robot/commands/SetElevatorL1Command.java
@@ -4,23 +4,43 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ElevatorPosition;
+import frc.robot.subsystems.ClawSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class SetElevatorL1Command extends InstantCommand {
-  ElevatorSubsystem m_elavatorSubsystem;
-  public SetElevatorL1Command(ElevatorSubsystem elavatorSubsystem) {
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class setElevatorL1Command extends Command {
+  /** Creates a new setElevatorL1Command. */
+  private ElevatorSubsystem m_elevatorSubsystem;
+  private boolean isFinished = false;
+  public setElevatorL1Command(ElevatorSubsystem elevatorSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
-    m_elavatorSubsystem = elavatorSubsystem;
+    m_elevatorSubsystem = elevatorSubsystem;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_elavatorSubsystem.setElevatorTargetPosition(ElevatorPosition.L1);
+    isFinished = false;
+    m_elevatorSubsystem.setElevatorTargetPosition(ElevatorPosition.L1);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(m_elevatorSubsystem.elevatorAtTargetPosition()){
+        isFinished = true;
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return isFinished;
   }
 }
